136f5186329760751eecb6505c83be68fc70a9eb
jingyan wang <wayenvan@outlook.com>
2025-09-16 23:29:10 +0000

    fix some bugs and add naieve prompt for translation

--- lm_eval/tasks/_michsethowusu/utils.py
+++ lm_eval/tasks/_michsethowusu/utils.py
@@ -1,4 +1,8 @@
-def doc_to_text(doc):
+def doc_to_text_naive(doc):
+    return "Translate English to Swahili: " + doc["eng"]
+
+
+def doc_to_text_template(doc):
     english = doc["eng"]
     prompt = (
         "Translate the following English text to Swahili\n"
